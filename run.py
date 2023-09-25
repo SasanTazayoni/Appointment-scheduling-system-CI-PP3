d1ba2b1dc49650029085336d7912207c3a6d01a0
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-09-25 16:16:47 +0000

    Create a function that populates the google sheet with dates according to the login time and date

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from google.oauth2.service_account import Credentials
 import colorama
 from colorama import Fore, Back
 colorama.init(autoreset=True)
+import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,13 +14,13 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('python_scheduling_system')
+SHEET = GSPREAD_CLIENT.open('appointment_scheduling_system')
 
 def login_prompt():
     """
     Prompt the user for a login and a password
     """
-    print(Fore.GREEN + 'Appointment Booking System')
+    print(Fore.GREEN + 'Appointment Booking System\n')
 
     print('Please enter the correct login details\n')
 
@@ -38,10 +39,35 @@ def login_prompt():
                 continue
             
             if login == "admin" and password == "password":
-                print(Fore.GREEN + "Login successful!")
+                print(Fore.GREEN + "Login successful!\n")
+                update_cell_dates()
                 return
             else:
                 print(Fore.RED + "Login failed. Please try again.\n")
                 break
+                
+def update_cell_dates():
+    
+    print(Fore.GREEN + 'Updating worksheet...')
+    worksheet_names = ["week1", "week2", "week3", "week4", "week5", "week6", "week7", "week8", "week9", "week10", "week11", "week12"]
+    current_datetime = datetime.datetime.now()
+    
+    for i, worksheet_name in enumerate(worksheet_names):
+        worksheet = SHEET.worksheet(worksheet_name)
+
+        days_since_monday = (current_datetime.weekday() - 0) % 7
+        start_date = current_datetime - datetime.timedelta(days=days_since_monday) + datetime.timedelta(weeks=i)
+
+        dates = [start_date + datetime.timedelta(days=j) for j in range(5)]
+
+        formatted_dates = [f"{date.strftime('%A')} ({date.strftime('%d-%m-%Y')})" for date in dates]
+
+        cell_values = worksheet.range("A2:A6")
+
+        for j, date in enumerate(formatted_dates):
+            cell_values[j].value = date
+
+        worksheet.update_cells(cell_values)
 
 login_prompt()
+
